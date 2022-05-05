2dc0f2bf07a2abafd509f2d5bdaac6f0a0d6e3ea
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-05-05 07:18:43 +0530

    query all converters benchmark added

---
+++ benchmarks/query_benchmarks/query_all_conv/benchmark.py
@@ -0,0 +1,12 @@
+from ...utils import bench_setup
+from .models import Converters
+
+class QueryAllConv:
+    
+    def setup(self):
+        bench_setup(migrate=True)
+        for i in range(0, 100):
+            Converters().save()
+    
+    def time_query_all_conv(self):
+        list(Converters.objects.iterator())
---
+++ benchmarks/query_benchmarks/query_all_conv/models.py
@@ -0,0 +1,10 @@
+from ...utils import bench_setup
+import datetime
+from django.db import models
+
+bench_setup()
+
+class Converters(models.Model):
+    """Both these fields have converters for sqlite"""
+    date = models.DateField(default=datetime.date.today)
+    decimal = models.DecimalField(default=0, max_digits=4, decimal_places=2)
