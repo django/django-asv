184d3f24e6020ef418485c4b992a7510e55aa6ef
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-05-06 07:23:40 +0530

    model validation benchmark added

--- benchmarks/model_benchmarks/model_validate/models.py
+++ benchmarks/model_benchmarks/model_validate/models.py
@@ -1,16 +1,12 @@
 from django.db import models
 from django.core.exceptions  import ValidationError
-from django.utils.translation import gettext_lazy as _
 from ...utils import bench_setup
 
 bench_setup()
 
 def validate_name(name):
     if name != 'abc':
-        raise ValidationError(
-            _('%(name)s is not abc'),
-            params={'name': name},
-        )
+        raise ValidationError
 
 class Song(models.Model):
     name = models.CharField(max_length=100, validators=[validate_name])
