c45b97ccc2be421a1747a182a71c826006bdd312
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-05-06 15:23:54 +0530

    form validation benchmark added

---
+++ benchmarks/form_benchmarks/form_validate/benchmark.py
@@ -0,0 +1,23 @@
+from ...utils import bench_setup
+from django.core.exceptions import ValidationError
+from django import forms
+
+def form_validator(title):
+    if title != 'hi':
+        raise ValidationError("title is not hi")
+
+class BookForm(forms.Form):
+    title = forms.CharField(max_length=100, validators=[form_validator])
+
+class FormValidate:
+
+    def setup(self):
+        bench_setup()
+        self.form = BookForm({'title':'hi'})
+        self.invalid_form = BookForm({'title':'abc'})
+
+    def time_form_validate(self):
+        self.form.is_valid()
+    
+    def time_form_invalid(self):
+        self.invalid_form.is_valid()
