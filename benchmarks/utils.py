bf73a10177a373e2841e14b11f1c9ed6db284f0f
deepakdinesh1123 <52698821+deepakdinesh1123@users.noreply.github.com>
2022-05-14 07:31:50 -0700

    utils.py added (#4)
    * utils.py added
    
    * migrate added to bench_setup

---
+++ benchmarks/utils.py
@@ -0,0 +1,20 @@
+import os
+import django
+from django.core.management import call_command, CommandError
+
+def bench_setup(migrate=False):
+    try:
+        os.environ["DJANGO_SETTINGS_MODULE"] = "benchmarks.settings"
+        django.setup()
+    except RuntimeError:
+        pass
+
+    if migrate is True:
+        call_command("migrate", run_syncdb=True, verbosity=0)
+        try:
+            call_command("loaddata", "initial_data", verbosity=0)
+        except CommandError as exc:
+            # Django 1.10+ raises if the file doesn't exist and not
+            # all benchmarks have files.
+            if "No fixture named" not in str(exc):
+                raise
