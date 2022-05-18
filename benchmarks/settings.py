135469ab7d811e07b5774570da37445755949c56
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-05-18 08:26:52 +0530

    setup benchmark added

--- benchmarks/settings.py
+++ benchmarks/settings.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "benchmarks",
+    "benchmarks.other_benchmarks.startup"
 ]
 
 SECRET_KEY = "NOT REALLY SECRET"
