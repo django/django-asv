ee3ff6e543a8449f39a36fb643ac013493d3019d
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-06-03 09:41:38 +0530

    setup method in template_compilation and template_render modified

--- benchmarks/template_benchmarks/template_render/benchmark.py
+++ benchmarks/template_benchmarks/template_render/benchmark.py
@@ -5,34 +5,22 @@ from django import VERSION, template
 
 from ...utils import bench_setup
 
-objects1 = [object(), object(), object(), object(), object()]
-objects2 = [object(), object(), object(), object(), object()]
-object1 = object()
-object2 = object()
-object3 = None
-num1 = 1
-num2 = 2
-boolean1 = True
-SCRIPT_CONTENT_URL = "/some/prefix"
-WEBSITE_DOMAIN = "http://www.somedomain.com"
-SHOW_ALT_HEADER = "True"
-
 
 class TemplateRender:
     def setup(self):
         bench_setup()
         self.context = {
-            "objects1": objects1,
-            "objects2": objects2,
-            "object1": object1,
-            "object2": object2,
-            "object3": object3,
-            "num1": num1,
-            "num2": num2,
-            "boolean1": boolean1,
-            "SCRIPT_CONTENT_URL": SCRIPT_CONTENT_URL,
-            "WEBSITE_DOMAIN": WEBSITE_DOMAIN,
-            "SHOW_ALT_HEADER": SHOW_ALT_HEADER,
+            "objects1": [object(), object(), object(), object(), object()],
+            "objects2": [object(), object(), object(), object(), object()],
+            "object1": object(),
+            "object2": object(),
+            "object3": None,
+            "num1": 1,
+            "num2": 2,
+            "boolean1": True,
+            "SCRIPT_CONTENT_URL": "/some/prefix",
+            "WEBSITE_DOMAIN": "http://www.somedomain.com",
+            "SHOW_ALT_HEADER": "True",
             "base_template": "base.html",
         }
 
