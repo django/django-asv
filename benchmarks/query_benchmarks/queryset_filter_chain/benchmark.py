8145e27f1d9453054e90d53b088860fc5c054d6b
Adam Johnson <me@adamj.eu>
2024-02-24 08:43:46 +0000

    Added system check benchmark (#80)

--- benchmarks/query_benchmarks/queryset_filter_chain/benchmark.py
+++ benchmarks/query_benchmarks/queryset_filter_chain/benchmark.py
@@ -12,11 +12,15 @@ class FilterChain:
         Book.objects.filter(title="Talent").filter(
             description__icontains="top performers"
         ).filter(author_name__startswith="Geoff").filter(
-            date_created__lt=datetime.datetime(year=2010, month=1, day=1)
+            date_created__lt=datetime.datetime(
+                year=2010, month=1, day=1, tzinfo=datetime.timezone.utc
+            )
         ).filter(
-            date_created__gte=datetime.date(year=2007, month=1, day=1)
+            date_created__gte=datetime.datetime(
+                year=2007, month=1, day=1, tzinfo=datetime.timezone.utc
+            )
         ).filter(
-            date_published=datetime.datetime.now()
+            date_published=datetime.datetime.now(tz=datetime.timezone.utc)
         ).filter(
             enabled=True
         )
