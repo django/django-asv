ec0b649356eccbaafcc6686f77ecab1a8c4dde82
Hisham Mahmood <hishammahmood41@gmail.com>
2024-03-30 15:09:04 +0500

    Generate keys and values in setup

--- benchmarks/cache_benchmarks/database_cache/benchmark.py
+++ benchmarks/cache_benchmarks/database_cache/benchmark.py
@@ -13,61 +13,61 @@ class DatabaseCacheBackend:
         random.seed(0)
 
         self.cache = caches["db"]
+        self.iterations = 100
         self.int_key = "int_key"
         self.cache.set(self.int_key, 0)
+        self.keys = [
+            "key_{}".format(random.randint(1, 500)) for _ in range(self.iterations)
+        ]
+        self.values = [
+            random.randint(1, 1024**1) * bytes([random.randint(0, 255)])
+            for _ in range(self.iterations)
+        ]
 
     def time_add(self):
-        for _ in range(100):
-            self.cache.add(self.random_key(), self.random_binary())
+        for key, value in zip(self.keys, self.values):
+            self.cache.add(key, value)
 
     def time_get(self):
-        for _ in range(100):
-            self.cache.get(self.random_key())
+        for key in self.keys:
+            self.cache.get(key)
 
     def time_set(self):
-        for _ in range(100):
-            self.cache.set(self.random_key(), self.random_binary())
+        for key, value in zip(self.keys, self.values):
+            self.cache.set(key, value)
 
     def time_get_or_set(self):
-        for _ in range(100):
-            self.cache.get_or_set(self.random_key(), self.random_binary())
+        for key, value in zip(self.keys, self.values):
+            self.cache.get_or_set(key, value)
 
     def time_touch(self):
-        for _ in range(100):
-            self.cache.touch(self.random_key())
+        for key in self.keys:
+            self.cache.touch(key)
 
     def time_delete(self):
-        for _ in range(100):
-            self.cache.delete(self.random_key())
+        for key in self.keys:
+            self.cache.delete(key)
 
     def time_get_many(self):
-        for _ in range(100):
-            self.cache.get_many([self.random_key() for x in range(100)])
+        for _ in range(self.iterations):
+            self.cache.get_many(self.keys)
 
     def time_set_many(self):
-        for _ in range(100):
-            self.cache.set_many(
-                {self.random_key(): self.random_binary() for x in range(100)}
-            )
+        for _ in range(self.iterations):
+            self.cache.set_many(dict(zip(self.keys, self.values)))
 
     def time_delete_many(self):
-        for _ in range(100):
-            self.cache.delete_many([self.random_key() for x in range(100)])
+        for _ in range(self.iterations):
+            self.cache.delete_many(self.keys)
 
     def time_clear(self):
-        for _ in range(100):
+        for _ in range(self.iterations):
             self.cache.clear()
 
     def time_incr(self):
-        for _ in range(100):
+        for _ in range(self.iterations):
             self.cache.incr(self.int_key)
 
     def time_decr(self):
-        for _ in range(100):
+        for _ in range(self.iterations):
             self.cache.decr(self.int_key)
-
-    def random_key(self):
-        return "key_{}".format(random.randint(1, 500))
-
-    def random_binary(self):
-        return random.randint(1, 1024**1) * random.randint(0, 255)
