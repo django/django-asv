9cd70a4fd57a0e0abf278fd9f202adb28576b9f4
Anna Makarudze <amakarudze@gmail.com>
2026-03-29 16:24:23 +0200

    Add benchmark to installed apps

--- benchmarks/query_benchmarks/query_utils_q_create/benchmark.py
+++ benchmarks/query_benchmarks/query_utils_q_create/benchmark.py
@@ -1,55 +1,28 @@
-import inspect
-from collections import Counter
+from django.db.models import Q
 
 from ...utils import bench_setup
-from django.db.models import Q
 from .models import Content
 
 
-class QueryQCreateCallsites:
+class QueryQCreate:
     def setup(self):
         bench_setup(migrate=True)
+        Content.objects.bulk_create(
+            [Content(a=i, b=i % 5, c=f"val-{i}") for i in range(1000)]
+        )
+        self.connector_OR = Q.OR
 
     def teardown(self):
         Content.objects.all().delete()
 
-    def time_q_internal_create_calls(self):
-        original_create = Q.create
-        call_count = 0
-        call_sites = Counter()
+    def time_q_create(self):
+        content_list = list(Content.objects.all())
+        Q.create(content_list, self.connector_OR)
 
-        def profiled_create(cls, *args, **kwargs):
-            nonlocal call_count, call_sites
-            call_count += 1
-
-            frame = inspect.stack()[1]
-            call_sites[(frame.filename, frame.lineno, frame.function)] += 1
-
-            # Normalize arguments
-            if args:
-                children = args[0]
-                connector = args[1] if len(args) > 1 else kwargs.get("connector", Q.AND)
-                negated = args[2] if len(args) > 2 else kwargs.get("negated", False)
-            else:
-                children = kwargs.get("children", [])
-                connector = kwargs.get("connector", Q.AND)
-                negated = kwargs.get("negated", False)
-
-            return original_create.__func__(cls, children, connector, negated)
-
-        Q.create = classmethod(profiled_create)
-
-        for i in range(2000):
-            q1 = Q(a=i)
-            q2 = Q(b=i % 5)
-            q3 = q1 & q2
-            q4 = q3 | Q(c=i)
-
-        Q.create = original_create
-
-        self.call_count = call_count
-        self.call_sites = call_sites
-        print("CALL COUNT:", call_count)
-        print("CALL SITES:", call_sites)
-
-        return {"call_count": call_count, "call_sites": call_sites}
+    def time_q_create_nested_loop(self):
+        content_list = [
+            Q.create(
+                [(content.a, content.b, content.c) for content in Content.objects.all()]
+            )
+        ]
+        Q.create(content_list, self.connector_OR)
