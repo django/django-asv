824eec514c3a7e6ce6e6f726b6a5ce5389cf14bd
Mariano Baragiola <mbaragiola@linux.com>
2026-08-21 14:34:19 -0300

    Update benchmarks/query_benchmarks/query_related_in/benchmark.py
    Co-authored-by: Jacob Walls <jacobtylerwalls@gmail.com>

--- benchmarks/query_benchmarks/query_related_in/benchmark.py
+++ benchmarks/query_benchmarks/query_related_in/benchmark.py
@@ -4,7 +4,7 @@ from .models import Book
 
 class QueryRelatedIn:
     def setup(self):
-        bench_setup()
+        bench_setup(migrate=True)
         self.author_ids = list(range(100000))
 
     def time_query_related_in(self):
