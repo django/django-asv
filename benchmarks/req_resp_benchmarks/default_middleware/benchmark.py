8aae25148a22d0d7fe1e29ac0f4f86eb38b76e7f
Jacob Walls <jacobtylerwalls@gmail.com>
2026-09-03 10:27:49 -0400

    Made time_asgi_handler() await a coroutine.
    Resolves:
    RuntimeWarning: coroutine 'DefaultMiddleWareBench.time_asgi_handler' was never awaited

--- benchmarks/req_resp_benchmarks/default_middleware/benchmark.py
+++ benchmarks/req_resp_benchmarks/default_middleware/benchmark.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from django.core.handlers.asgi import ASGIHandler
 from django.core.handlers.wsgi import WSGIHandler
 from django.test import AsyncRequestFactory, RequestFactory
@@ -14,7 +16,11 @@ class DefaultMiddleWareBench:
 
         self.async_req_factory = AsyncRequestFactory()
         self.asgi_handler = ASGIHandler()
-        self.asgi_handler.load_middleware()
+        self.asgi_handler.load_middleware(is_async=True)
+        self.event_loop = asyncio.new_event_loop()
+
+    def teardown(self):
+        self.event_loop.close()
 
     def time_wsgi_handler(self):
         self.wsgi_handler.get_response(self.req_factory.get("/inx-pg"))
@@ -23,9 +29,22 @@ class DefaultMiddleWareBench:
         self.wsgi_handler.get_response(self.req_factory.get("/inx-pg"))
         self.wsgi_handler.get_response(self.req_factory.get("/inx-pg"))
 
-    async def time_asgi_handler(self):
-        await self.asgi_handler.get_response(self.async_req_factory.get("/inx-pg"))
-        await self.asgi_handler.get_response(self.async_req_factory.get("/inx-pg"))
-        await self.asgi_handler.get_response(self.async_req_factory.get("/inx-pg"))
-        await self.asgi_handler.get_response(self.async_req_factory.get("/inx-pg"))
-        await self.asgi_handler.get_response(self.async_req_factory.get("/inx-pg"))
+    async def _get_asgi_responses(self):
+        await self.asgi_handler.get_response_async(
+            self.async_req_factory.get("/inx-pg")
+        )
+        await self.asgi_handler.get_response_async(
+            self.async_req_factory.get("/inx-pg")
+        )
+        await self.asgi_handler.get_response_async(
+            self.async_req_factory.get("/inx-pg")
+        )
+        await self.asgi_handler.get_response_async(
+            self.async_req_factory.get("/inx-pg")
+        )
+        await self.asgi_handler.get_response_async(
+            self.async_req_factory.get("/inx-pg")
+        )
+
+    def time_asgi_handler(self):
+        self.event_loop.run_until_complete(self._get_asgi_responses())
